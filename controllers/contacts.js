82d56c8ac2ff528e3420c4147d08f6c261f03f23
Devy Salomon <devysalomon@gmail.com>
2024-01-15 19:55:51 -0500

    new elements

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -18,7 +18,53 @@ const getSingle = async (req, res) => {
     });
 };
 
+const createUser = async (req, res) => {
+    const contact = {
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        email : req.body.email,
+        favoriteColor : req.body.favoriteColor,
+        birthday : req.body.birthday
+        };
+    const response = await mongodb.getDatabase().db('contacts').collection('contacts').insertOne(contact);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error courred while updating the user.')
+    }
+};
+
+const updateUser = async (req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const contact = {
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        email : req.body.email,
+        favoriteColor : req.body.favoriteColor,
+        birthday : req.body.birthday
+        };
+    const response = await mongodb.getDatabase().db('contacts').collection('contacts').replaceOne({_id:contactId}, contact);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error courred while updating the user.')
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db('contacts').collection('contacts').remove({_id:contactId}, true);
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error courred while updating the user.')
+    }
+};
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 }
