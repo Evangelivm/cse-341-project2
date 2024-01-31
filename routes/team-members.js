fbd63f294408a612a5eabaf84018f3bf78dc68a9
Devy Salomon <devysalomon@gmail.com>
2024-01-31 00:01:58 -0500

    new

--- routes/team-members.js
+++ routes/team-members.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const teamController = require('../controllers/team-members');
+const validation = require('../middleware/validate');
 
 router.get('/', teamController.getAll);
 router.get('/:id', teamController.getSingle);
-router.post('/', teamController.createTeamMem);
-router.put('/:id', teamController.updateTeamMem);
-router.delete('/:id', teamController.deleteTeamMem);
+router.post('/', validation.saveTeamMem,teamController.createTeamMem);
+router.put('/:id', validation.saveTeamMem, teamController.updateTeamMem);
+router.delete('/:id', validation.saveTeamMem, teamController.deleteTeamMem);
 
 
 module.exports = router;
